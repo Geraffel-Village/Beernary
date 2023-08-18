13d7dd08bf298650dd5ffb42d56b17688792b33b
Simon Peters <19429211+sp3t3rs@users.noreply.github.com>
2023-08-18 02:17:17 +0200

    Fix webhook ID

--- beernary/modules/reader.py
+++ beernary/modules/reader.py
@@ -265,7 +265,7 @@ class HTTPReader(IdentityReader):
     def read_rfid():
         if HTTPReader.identity_received:
             HTTPReader.identity_received = False
-            return HTTPReader.identity_name
+            return HTTPReader.received_identity_id
         else:
             return None
 
